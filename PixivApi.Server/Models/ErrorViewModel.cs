84998c7a490b9488953657adf838be5c3c00608a
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-02-24 20:29:05 +0900

    more simple

--- PixivApi.Server/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PixivApi.Server.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ PixivApi.Site/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.Extensions.FileProviders;
+using PixivApi.Core;
+
+using var cancellationTokenSource = new CancellationTokenSource();
+Console.CancelKeyPress += Console_CancelKeyPress;
+
+void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+{
+    cancellationTokenSource.Cancel();
+    e.Cancel = true;
+}
+
+var token = cancellationTokenSource.Token;
+var configSettings = await IOUtility.JsonDeserializeAsync<ConfigSettings>(IOUtility.GetConfigFileNameDependsOnEnvironmentVariable(), token).ConfigureAwait(false);
+if (string.IsNullOrWhiteSpace(configSettings?.RefreshToken))
+{
+    return;
+}
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseKestrel();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+var originalSharedProvider = new Microsoft.AspNetCore.StaticFiles.Infrastructure.SharedOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.GetFullPath(configSettings.OriginalFolder)),
+    RequestPath = "/Original",
+};
+var thumbnailSharedProvider = new Microsoft.AspNetCore.StaticFiles.Infrastructure.SharedOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.GetFullPath(configSettings.ThumbnailFolder)),
+    RequestPath = "/Thumbnail",
+};
+var ugoiraSharedProvider = new Microsoft.AspNetCore.StaticFiles.Infrastructure.SharedOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.GetFullPath(configSettings.UgoiraFolder)),
+    RequestPath = "/Ugoira",
+};
+
+app.UseStaticFiles(new StaticFileOptions(originalSharedProvider));
+app.UseStaticFiles(new StaticFileOptions(thumbnailSharedProvider));
+app.UseStaticFiles(new StaticFileOptions(ugoiraSharedProvider));
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDirectoryBrowser(new DirectoryBrowserOptions(originalSharedProvider));
+    app.UseDirectoryBrowser(new DirectoryBrowserOptions(thumbnailSharedProvider));
+    app.UseDirectoryBrowser(new DirectoryBrowserOptions(ugoiraSharedProvider));
+}
+
+app.Run();
