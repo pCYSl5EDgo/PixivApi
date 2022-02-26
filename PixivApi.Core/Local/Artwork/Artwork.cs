45ec42fccaba4ce08ab36bee67eb1919e4a207f5
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-02-26 20:14:33 +0900

    fix thumbnail name

--- PixivApi.Core/Local/Artwork/Artwork.cs
+++ PixivApi.Core/Local/Artwork/Artwork.cs
@@ -123,7 +123,7 @@ public sealed partial class Artwork : IOverwrite<Artwork>, IEquatable<Artwork>
         AddDateToUrl(ref handler);
         handler.AppendFormatted('/');
         handler.AppendFormatted(Id);
-        handler.AppendLiteral("_ugoira0.jpg");
+        handler.AppendLiteral("_square1200.jpg");
         return handler.ToStringAndClear();
     }
 
