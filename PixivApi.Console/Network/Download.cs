7ee52ae7003cd0e08176d561b4431270871dfa70
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-02-26 20:12:24 +0900

    add utility command `download` `download` determines which to download by reading filter.jsonc

--- PixivApi.Console/Network/Download.cs
+++ PixivApi.Console/Network/Download.cs
@@ -5,6 +5,139 @@ namespace PixivApi.Console;
 
 public partial class NetworkClient
 {
+    [Command("download")]
+    public async ValueTask<int> DownloadFileFromDatabaseAsync(
+        [Option(0, $"input {ArgumentDescriptions.DatabaseDescription}")] string path,
+        [Option(1, ArgumentDescriptions.FilterDescription)] string filter,
+        [Option("g")] ulong gigaByteCount = 2UL,
+        [Option("mask")] int maskPowerOf2 = 10,
+        bool pipe = false
+    )
+    {
+        var token = Context.CancellationToken;
+        var artworkFilter = await IOUtility.JsonDeserializeAsync<ArtworkFilter>(filter, token).ConfigureAwait(false);
+        if (artworkFilter is not { FileExistanceFilter: { } fileFilter })
+        {
+            return -1;
+        }
+
+        var shouldDownloadOriginal = fileFilter.Original != FileExistanceType.None;
+        var shouldDownloadThumbnail = fileFilter.Thumbnail != FileExistanceType.None;
+        if (!shouldDownloadOriginal && !shouldDownloadThumbnail)
+        {
+            return -1;
+        }
+
+        var (database, artworks) = await PrepareDownloadFileAsync(path, artworkFilter, configSettings.OriginalFolder, gigaByteCount).ConfigureAwait(false);
+        if (artworks is null)
+        {
+            return -1;
+        }
+
+        var downloadItemCount = 0;
+        var alreadyCount = 0;
+        var updateFlag = false;
+        var machine = new DownloadAsyncMachine(this, database, pipe, token);
+        try
+        {
+            await foreach (var artwork in artworks)
+            {
+                if (token.IsCancellationRequested || (machine.DownloadByteCount >> 30) >= gigaByteCount)
+                {
+                    break;
+                }
+
+                machine.Initialize();
+                if (artwork.Type == ArtworkType.Ugoira)
+                {
+                    if (shouldDownloadOriginal)
+                    {
+                        var ugoiraZipFile = DownloadAsyncMachine.PrepareFileInfo(configSettings.UgoiraFolder, artwork.Id, artwork.GetZipFileName());
+                        if (!ugoiraZipFile.Exists && !await machine.DownloadAsync(ugoiraZipFile, artwork, artwork.GetZipUrl).ConfigureAwait(false))
+                        {
+                            goto FAIL;
+                        }
+                    }
+                    
+                    if (shouldDownloadThumbnail)
+                    {
+                        var thumbnailFile = DownloadAsyncMachine.PrepareFileInfo(configSettings.ThumbnailFolder, artwork.Id, artwork.GetThumbnailFileName(0));
+                        if (!thumbnailFile.Exists && !await machine.DownloadAsync(thumbnailFile, artwork, artwork.GetUgoiraThumbnailUrl).ConfigureAwait(false))
+                        {
+                            goto FAIL;
+                        }
+                    }
+                }
+
+                for (uint pageIndex = 0; pageIndex < artwork.PageCount; pageIndex++)
+                {
+                    if (shouldDownloadOriginal)
+                    {
+                        var pageFile = DownloadAsyncMachine.PrepareFileInfo(configSettings.OriginalFolder, artwork.Id, artwork.GetOriginalFileName(pageIndex));
+                        if (pageFile.Exists)
+                        {
+                            continue;
+                        }
+
+                        if (!await machine.DownloadAsync(pageFile, artwork, pageIndex, artwork.GetOriginalUrl).ConfigureAwait(false))
+                        {
+                            goto FAIL;
+                        }
+                    }
+                    
+                    if (shouldDownloadThumbnail)
+                    {
+                        var pageFile = DownloadAsyncMachine.PrepareFileInfo(configSettings.ThumbnailFolder, artwork.Id, artwork.GetThumbnailFileName(pageIndex));
+                        if (pageFile.Exists)
+                        {
+                            continue;
+                        }
+
+                        if (!await machine.DownloadAsync(pageFile, artwork, pageIndex, artwork.GetThumbnailUrl).ConfigureAwait(false))
+                        {
+                            goto FAIL;
+                        }
+                    }
+                }
+
+                if (machine.IsUpdated)
+                {
+                    updateFlag = true;
+                }
+
+                ++downloadItemCount;
+                continue;
+
+            FAIL:
+                if (machine.IsUpdated)
+                {
+                    updateFlag = true;
+                }
+
+                ++alreadyCount;
+            }
+        }
+        finally
+        {
+            if (!pipe)
+            {
+                logger.LogInformation($"Item: {downloadItemCount}, File: {machine.DownloadFileCount}, Already: {alreadyCount}, Transfer: {ToDisplayableByteAmount(machine.DownloadByteCount)}");
+            }
+
+            if (updateFlag)
+            {
+                await IOUtility.MessagePackSerializeAsync(path, database, FileMode.Create).ConfigureAwait(false);
+            }
+
+            if (alreadyCount != 0)
+            {
+                await LocalClient.ClearAsync(logger, configSettings, maskPowerOf2, Context.CancellationToken).ConfigureAwait(false);
+            }
+        }
+
+        return 0;
+    }
+
     [Command("download-original")]
     public async ValueTask<int> DownloadOriginalFileFromDatabaseAsync(
         [Option(0, $"input {ArgumentDescriptions.DatabaseDescription}")] string path,
