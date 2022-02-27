33bd0f007615c13d2032df2daafdf276cb381db9
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-02-27 21:07:04 +0900

    ProcessX ffmpeg execution

--- PixivApi.Console/Local/Ugoira.cs
+++ PixivApi.Console/Local/Ugoira.cs
@@ -27,6 +27,7 @@ public partial class LocalClient
 
         var extension = codec.GetExtension();
         var template = new UgoiraFfmpegTemplate();
+        var twoTasks = new Task[2];
         foreach (var artwork in database.ArtworkDictionary.Values)
         {
             if (token.IsCancellationRequested)
@@ -41,9 +42,9 @@ public partial class LocalClient
 
             template.Frames = artwork.UgoiraFrames;
             var hashPath = Path.Combine(configSettings.UgoiraFolder, IOUtility.GetHashPath(artwork.Id));
-            var partialPath = Path.Combine(hashPath, artwork.GetUgoiraZipFileNameWithoutExtension());
+            var partialPath = Path.Combine(hashPath, artwork.Id.ToString());
+            var zipPath = Path.Combine(hashPath, artwork.GetUgoiraZipFileName());
             var destPath = partialPath + extension;
-            var zipPath = partialPath + ".zip";
             if (File.Exists(destPath) || !File.Exists(zipPath))
             {
                 continue;
@@ -54,44 +55,26 @@ public partial class LocalClient
                 return;
             }
 
+            var textName = $"{artwork.Id}.txt";
             template.Directory = Path.GetFullPath(partialPath);
             Directory.CreateDirectory(template.Directory);
             try
             {
-                bool isCompressed;
-                using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Read))
-                {
-                    archive.ExtractToDirectory(template.Directory, true);
-                    if (archive.Entries.Count == 0)
-                    {
-                        isCompressed = true;
-                    }
-                    else
-                    {
-                        var entry = archive.Entries[0];
-                        isCompressed = entry.CompressedLength < entry.Length;
-                    }
-                }
-
-                if (!isCompressed)
-                {
-                    File.Delete(zipPath);
-                    ZipFile.CreateFromDirectory(template.Directory, zipPath, CompressionLevel.SmallestSize, false);
-                }
+                Decompress(template.Directory, zipPath);
 
                 var builder = ZString.CreateUtf8StringBuilder();
-                var textName = $"{artwork.Id}.txt";
                 try
                 {
                     template.TransformAppend(ref builder);
-                    IOUtility.WriteToFile(Path.Combine(partialPath, textName), builder.AsSpan());
+                    IOUtility.WriteToFile(textName, builder.AsSpan());
                 }
                 finally
                 {
                     builder.Dispose();
                 }
 
-                static string CalcCommand(UgoiraCodec ugoiraCodec, string textName, string destPath)
+                var tempDest = $"{artwork.Id}{extension}";
+                string CalcCommand()
                 {
                     DefaultInterpolatedStringHandler handler = $"ffmpeg";
                     if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
@@ -99,31 +82,123 @@ public partial class LocalClient
                         handler.AppendLiteral(".exe");
                     }
 
-                    handler.AppendLiteral(" -f concat -i ");
-                    handler.AppendFormatted(textName);
+                    handler.AppendLiteral(" -f concat -safe 0 -i ");
+                    handler.AppendLiteral(textName);
                     handler.AppendLiteral(" -c:v ");
-                    handler.AppendLiteral(ugoiraCodec.GetCodecTextForFfmpeg());
-                    handler.AppendLiteral(" -r 30 ");
+                    handler.AppendLiteral(codec.GetCodecTextForFfmpeg());
+                    handler.AppendLiteral(" -r ");
 
-                    if (ugoiraCodec == UgoiraCodec.h264)
+                    if (TryCalculateFps(artwork.UgoiraFrames, out var fps))
+                    {
+                        handler.AppendFormatted(fps);
+                    }
+                    else
+                    {
+                        handler.AppendFormatted(60);
+                    }
+
+                    handler.AppendLiteral(" ");
+                    if (codec == UgoiraCodec.h264)
                     {
                         handler.AppendLiteral("-pix_fmt yuv420p ");
                     }
 
-                    handler.AppendLiteral(destPath);
+                    handler.AppendLiteral(tempDest);
                     return handler.ToStringAndClear();
                 }
 
-                var command = CalcCommand(codec, textName, destPath);
-                await foreach (var output in ProcessX.StartAsync(command, workingDirectory: hashPath))
+                var command = CalcCommand();
+                logger.LogInformation(command);
+                var (_, standardOutput, standardError) = ProcessX.GetDualAsyncEnumerable(command);
+                twoTasks[0] = Task.Run(async () =>
                 {
-                    logger.LogInformation(output);
+                    await foreach (var item in standardOutput)
+                    {
+                        logger.LogInformation(item);
+                    }
+                });
+                twoTasks[1] = Task.Run(async () =>
+                {
+                    await foreach (var item in standardError)
+                    {
+                        logger.LogWarning(item);
+                    }
+                });
+
+                await Task.WhenAll(twoTasks).ConfigureAwait(false);
+                if (File.Exists(tempDest))
+                {
+                    File.Move(tempDest, destPath);
                 }
             }
             finally
             {
+                if (!string.IsNullOrWhiteSpace(textName) && File.Exists(textName))
+                {
+                    File.Delete(textName);
+                }
+
                 Directory.Delete(template.Directory, true);
             }
+        }
+    }
+
+    private static bool TryCalculateFps(ushort[] frames, out uint framePerSecond)
+    {
+        if (frames.Length == 0)
+        {
+            goto FAIL;
+        }
+
+        var first = frames[0];
+        if (first == 0)
+        {
+            goto FAIL;
+        }
+
+        framePerSecond = (uint)(1000 / first);
+        if (framePerSecond == 0)
+        {
+            goto FAIL;
+        }
+
+        for (var i = 1; i < frames.Length; i++)
+        {
+            if (frames[i] != first)
+            {
+                goto FAIL;
+            }
+        }
+
+        return true;
+
+    FAIL:
+        Unsafe.SkipInit(out framePerSecond);
+        return false;
+    }
+
+
+    private static void Decompress(string directory, string zipPath)
+    {
+        bool isCompressed;
+        using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Read))
+        {
+            archive.ExtractToDirectory(directory, true);
+            if (archive.Entries.Count == 0)
+            {
+                isCompressed = true;
+            }
+            else
+            {
+                var entry = archive.Entries[0];
+                isCompressed = entry.CompressedLength < entry.Length;
+            }
+        }
+
+        if (!isCompressed)
+        {
+            File.Delete(zipPath);
+            ZipFile.CreateFromDirectory(directory, zipPath, CompressionLevel.SmallestSize, false);
         }
     }
 }
