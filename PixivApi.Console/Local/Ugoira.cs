d75f0ecb177f4c7286f4bde151d3f0c8e4856a0d
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-02-27 14:41:02 +0900

    convert ugoira zip to mp4

---
+++ PixivApi.Console/Local/Ugoira.cs
@@ -0,0 +1,129 @@
+﻿using Cysharp.Diagnostics;
+using PixivApi.Core;
+using PixivApi.Core.Local;
+using System.IO.Compression;
+
+namespace PixivApi.Console;
+
+public partial class LocalClient
+{
+    [Command("create-ugoira")]
+    public async ValueTask CreateUgoiraAsync(
+        [Option(0, ArgumentDescriptions.DatabaseDescription)] string path,
+        [Option(1, "codec")] UgoiraCodec codec
+    )
+    {
+        var token = Context.CancellationToken;
+        if (token.IsCancellationRequested)
+        {
+            return;
+        }
+
+        var database = await IOUtility.MessagePackDeserializeAsync<DatabaseFile>(path, token).ConfigureAwait(false);
+        if (database is null)
+        {
+            return;
+        }
+
+        var extension = codec.GetExtension();
+        var template = new UgoiraFfmpegTemplate();
+        foreach (var artwork in database.ArtworkDictionary.Values)
+        {
+            if (token.IsCancellationRequested)
+            {
+                return;
+            }
+
+            if (artwork.Type != ArtworkType.Ugoira || artwork.UgoiraFrames is not { Length: > 0 })
+            {
+                continue;
+            }
+
+            template.Frames = artwork.UgoiraFrames;
+            var hashPath = Path.Combine(configSettings.UgoiraFolder, IOUtility.GetHashPath(artwork.Id));
+            var partialPath = Path.Combine(hashPath, artwork.GetZipFileNameWithoutExtension());
+            var destPath = partialPath + extension;
+            var zipPath = partialPath + ".zip";
+            if (File.Exists(destPath) || !File.Exists(zipPath))
+            {
+                continue;
+            }
+
+            if (token.IsCancellationRequested)
+            {
+                return;
+            }
+
+            template.Directory = Path.GetFullPath(partialPath);
+            Directory.CreateDirectory(template.Directory);
+            try
+            {
+                bool isCompressed;
+                using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Read))
+                {
+                    archive.ExtractToDirectory(template.Directory, true);
+                    if (archive.Entries.Count == 0)
+                    {
+                        isCompressed = true;
+                    }
+                    else
+                    {
+                        var entry = archive.Entries[0];
+                        isCompressed = entry.CompressedLength < entry.Length;
+                    }
+                }
+
+                if (!isCompressed)
+                {
+                    File.Delete(zipPath);
+                    ZipFile.CreateFromDirectory(template.Directory, zipPath, CompressionLevel.SmallestSize, false);
+                }
+
+                var builder = ZString.CreateUtf8StringBuilder();
+                var textName = $"{artwork.Id}.txt";
+                try
+                {
+                    template.TransformAppend(ref builder);
+                    IOUtility.WriteToFile(Path.Combine(partialPath, textName), builder.AsSpan());
+                }
+                finally
+                {
+                    builder.Dispose();
+                }
+
+                static string CalcCommand(UgoiraCodec ugoiraCodec, string textName, string destPath)
+                {
+                    DefaultInterpolatedStringHandler handler = $"ffmpeg";
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        handler.AppendLiteral(".exe");
+                    }
+
+                    handler.AppendLiteral(" -f concat -i ");
+                    handler.AppendFormatted(textName);
+                    handler.AppendLiteral(" -c:v ");
+                    handler.AppendLiteral(ugoiraCodec.GetCodecTextForFfmpeg());
+                    handler.AppendLiteral(" -r 30 ");
+
+                    if (ugoiraCodec == UgoiraCodec.h264)
+                    {
+                        handler.AppendLiteral("-pix_fmt yuv420p ");
+                    }
+
+                    handler.AppendLiteral(destPath);
+                    return handler.ToStringAndClear();
+                }
+
+                var command = CalcCommand(codec, textName, destPath);
+                await foreach (var output in ProcessX.StartAsync(command, workingDirectory: hashPath))
+                {
+                    logger.LogInformation(output);
+                }
+            }
+            finally
+            {
+                Directory.Delete(template.Directory, true);
+            }
+        }
+    }
+}
---
+++ PixivApi.Core/Utility/UgoiraCodec.cs
@@ -0,0 +1,20 @@
+﻿namespace PixivApi.Core;
+
+public enum UgoiraCodec
+{
+    av1,
+    h264
+}
+
+public static class UgoiraCodecExtensions
+{
+#pragma warning disable IDE0060
+    public static string GetExtension(this UgoiraCodec ugoiraCodec) => ".mp4";
+#pragma warning restore IDE0060
+
+    public static string GetCodecTextForFfmpeg(this UgoiraCodec ugoiraCodec) => ugoiraCodec switch
+    {
+        UgoiraCodec.av1 => "libaom-av1",
+        UgoiraCodec.h264 => "libx264",
+    };
+}
