b3ac1b737603601455ef69275e9b9a45673e414d
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-01 04:29:03 +0900

    add command encode

---
+++ PixivApi.Console/Local/Encode.cs
@@ -0,0 +1,71 @@
+﻿using PixivApi.Core;
+using PixivApi.Core.Local;
+
+namespace PixivApi.Console;
+
+public partial class LocalClient
+{
+    [Command("encode", "")]
+    public async ValueTask EncodeAsync(
+        [Option(0, ArgumentDescriptions.DatabaseDescription)] string path,
+        [Option(1, ArgumentDescriptions.FilterDescription)] string? filter = null,
+        [Option("o")] bool original = false,
+        [Option("t")] bool thumbanil = false,
+        [Option("u")] bool ugoira = false
+    )
+    {
+        if (!original && !thumbanil && !ugoira)
+        {
+            return;
+        }
+
+        var token = Context.CancellationToken;
+        var database = await IOUtility.MessagePackDeserializeAsync<DatabaseFile>(path, token).ConfigureAwait(false);
+        if (database is null)
+        {
+            return;
+        }
+
+        var artworkFilter = string.IsNullOrWhiteSpace(filter) ? null : await IOUtility.JsonDeserializeAsync<ArtworkFilter>(filter, token).ConfigureAwait(false);
+        if (artworkFilter is null)
+        {
+            foreach (var artwork in database.ArtworkDictionary.Values)
+            {
+                if (original && converter.OriginalConverter is { } originalConverter)
+                {
+                    _ = await originalConverter.TryConvertAsync(artwork, logger, token).ConfigureAwait(false);
+                }
+
+                if (thumbanil && converter.ThumbnailConverter is { } thumbnailConverter)
+                {
+                    _ = await thumbnailConverter.TryConvertAsync(artwork, logger, token).ConfigureAwait(false);
+                }
+
+                if (ugoira && converter.UgoiraZipConverter is { } ugoiraZipConverter)
+                {
+                    _ = await ugoiraZipConverter.TryConvertAsync(artwork, logger, token).ConfigureAwait(false);
+                }
+            }
+        }
+        else
+        {
+            foreach (var artwork in await FilterExtensions.CreateEnumerableWithoutFileExistanceFilterAsync(database, artworkFilter, token).ConfigureAwait(false))
+            {
+                if (original && converter.OriginalConverter is { } originalConverter)
+                {
+                    _ = await originalConverter.TryConvertAsync(artwork, logger, token).ConfigureAwait(false);
+                }
+
+                if (thumbanil && converter.ThumbnailConverter is { } thumbnailConverter)
+                {
+                    _ = await thumbnailConverter.TryConvertAsync(artwork, logger, token).ConfigureAwait(false);
+                }
+
+                if (ugoira && converter.UgoiraZipConverter is { } ugoiraZipConverter)
+                {
+                    _ = await ugoiraZipConverter.TryConvertAsync(artwork, logger, token).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
