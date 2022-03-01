f0b30a7b97b127e92f8a5a0f8813555bd575dfa7
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-01 14:20:21 +0900

    SetTitle

--- PixivApi.Console.Utility/VirtualCodes.cs
+++ PixivApi.Console.Utility/VirtualCodes.cs
@@ -1,8 +1,11 @@
-﻿namespace PixivApi.Console;
+﻿using System.Runtime.CompilerServices;
+
+namespace PixivApi.Console;
 
 public static class VirtualCodes
 {
     public const string ESC = "\u001b";
+    public const string TitleTeminator = "\u001b\u005c";
 
     public const string BrightRedColor = $"{ESC}[91m";
     public const string BrightGreenColor = $"{ESC}[92m";
@@ -15,4 +18,21 @@ public static class VirtualCodes
 
     public const string UseAltDisplayBuffer = $"{ESC}[?1049h";
     public const string UseMainDisplayBuffer = $"{ESC}[?1049l";
+
+    public const string SavePosition = $"{ESC}7";
+    public const string LoadPosition = $"{ESC}8";
+
+    public static void SetTitle(ref DefaultInterpolatedStringHandler handler, ReadOnlySpan<char> title)
+    {
+        handler.AppendLiteral($"{ESC}]0;");
+        handler.AppendFormatted(title);
+        handler.AppendLiteral(TitleTeminator);
+    }
+
+    public static string SetTitle(ReadOnlySpan<char> title)
+    {
+        var handler = new DefaultInterpolatedStringHandler();
+        SetTitle(ref handler, title);
+        return handler.ToStringAndClear();
+    }
 }
