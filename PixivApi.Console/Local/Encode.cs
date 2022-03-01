0c5fbd31ca0ded800fd11adc61f20db89e3897ae
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-01 14:34:37 +0900

    add delete unneccessary encoded command

--- PixivApi.Console/Local/Encode.cs
+++ PixivApi.Console/Local/Encode.cs
@@ -1,5 +1,4 @@
-﻿using Cysharp.Diagnostics;
-using PixivApi.Core;
+﻿using PixivApi.Core;
 using PixivApi.Core.Local;
 
 namespace PixivApi.Console;
@@ -67,6 +66,72 @@ public partial class LocalClient
                 if (ugoira && converter.UgoiraZipConverter is { } ugoiraZipConverter)
                 {
                     _ = await ugoiraZipConverter.TryConvertAsync(artwork, logger, token).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+
+    [Command("delete-unneccessary-encoded", "")]
+    public async ValueTask DeleteUnneccessaryEncodedAsync(
+        [Option(0, ArgumentDescriptions.DatabaseDescription)] string path,
+        [Option(1, ArgumentDescriptions.FilterDescription)] string? filter = null,
+        [Option("o")] bool original = false,
+        [Option("t")] bool thumbanil = false,
+        [Option("u")] bool ugoira = false
+    )
+    {
+        if (!original && !thumbanil && !ugoira)
+        {
+            return;
+        }
+
+        var token = Context.CancellationToken;
+        var database = await IOUtility.MessagePackDeserializeAsync<DatabaseFile>(path, token).ConfigureAwait(false);
+        if (database is null)
+        {
+            return;
+        }
+
+        var artworkFilter = string.IsNullOrWhiteSpace(filter) ? null : await IOUtility.JsonDeserializeAsync<ArtworkFilter>(filter, token).ConfigureAwait(false);
+        if (artworkFilter is null)
+        {
+            foreach (var artwork in database.ArtworkDictionary.Values)
+            {
+                VirtualCodes.SetTitle($"{artwork.Id}");
+                if (original && converter.OriginalConverter is { } originalConverter)
+                {
+                    originalConverter.DeleteUnneccessaryOriginal(artwork, logger);
+                }
+
+                if (thumbanil && converter.ThumbnailConverter is { } thumbnailConverter)
+                {
+                    thumbnailConverter.DeleteUnneccessaryOriginal(artwork, logger);
+                }
+
+                if (ugoira && converter.UgoiraZipConverter is { } ugoiraZipConverter)
+                {
+                    ugoiraZipConverter.DeleteUnneccessaryOriginal(artwork, logger);
+                }
+            }
+        }
+        else
+        {
+            foreach (var artwork in await FilterExtensions.CreateEnumerableWithoutFileExistanceFilterAsync(database, artworkFilter, token).ConfigureAwait(false))
+            {
+                VirtualCodes.SetTitle($"{artwork.Id}");
+                if (original && converter.OriginalConverter is { } originalConverter)
+                {
+                    originalConverter.DeleteUnneccessaryOriginal(artwork, logger);
+                }
+
+                if (thumbanil && converter.ThumbnailConverter is { } thumbnailConverter)
+                {
+                    thumbnailConverter.DeleteUnneccessaryOriginal(artwork, logger);
+                }
+
+                if (ugoira && converter.UgoiraZipConverter is { } ugoiraZipConverter)
+                {
+                    ugoiraZipConverter.DeleteUnneccessaryOriginal(artwork, logger);
                 }
             }
         }
