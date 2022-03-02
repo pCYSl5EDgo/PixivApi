99ea12d76bd7509e98a9b7ac8cf885c4d671b854
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-03 02:17:27 +0900

    single update mechanism

---
+++ PixivApi.Core/Utility/SingleUpdater.cs
@@ -0,0 +1,29 @@
+﻿namespace PixivApi.Core;
+
+public interface ISingleUpdater<T>
+{
+    ref Task<T>? GetTask { get; }
+
+    Task<T> UpdateAsync(CancellationToken token);
+}
+
+public static class SingleUpdateUtility
+{
+    public static async Task<T> GetAsync<T>(ISingleUpdater<T> singleUpdater, CancellationToken token)
+    {
+        var current = singleUpdater.GetTask;
+        if (current is not null && ReferenceEquals(Interlocked.CompareExchange(ref singleUpdater.GetTask, null, current), current))
+        {
+            singleUpdater.GetTask = singleUpdater.UpdateAsync(token);
+        }
+        else
+        {
+            while (singleUpdater.GetTask is null)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1d), token).ConfigureAwait(false);
+            }
+        }
+
+        return await singleUpdater.GetTask.ConfigureAwait(false);
+    }
+}
