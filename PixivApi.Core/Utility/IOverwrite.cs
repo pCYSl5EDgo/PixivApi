3e2e47fdd87f03165e08957752f60cc10d5b87e9
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-04 04:11:29 +0900

    remove IOverwrite

--- PixivApi.Core/Utility/IOverwrite.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PixivApi.Core;
-
-public interface IOverwrite<T>
-{
-    void Overwrite(T source);
-}
--- PixivApi.Core/Utility/OverwriteExtensions.cs
+++ PixivApi.Core/Utility/OverwriteExtensions.cs
@@ -2,20 +2,13 @@
 
 public static class OverwriteExtensions
 {
-    public static void Overwrite<T>([NotNullIfNotNull("value"), NotNullIfNotNull("source")] ref T? value, T? source) where T : class
+    public static void Overwrite<T>([NotNullIfNotNull("value"), NotNullIfNotNull("source")] ref T? destination, T? source) where T : class
     {
         if (source is null)
         {
             return;
         }
 
-        if (value is IOverwrite<T> overwrite)
-        {
-            overwrite.Overwrite(source);
-        }
-        else
-        {
-            value = source;
-        }
+        destination = source;
     }
 }
