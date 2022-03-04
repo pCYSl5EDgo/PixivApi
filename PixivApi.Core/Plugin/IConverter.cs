ae419ed90762efbe41df9e188f9d5902a19ce1a9
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-05 06:44:39 +0900

    not Artwork but FileInfo

--- PixivApi.Core/Plugin/IConverter.cs
+++ PixivApi.Core/Plugin/IConverter.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
-using PixivApi.Core.Local;
 
 namespace PixivApi.Core.Plugin;
 
 public interface IConverter : IPlugin
 {
-    public ValueTask<bool> TryConvertAsync(Artwork artwork, ILogger? logger, CancellationToken cancellationToken);
-
-    public void DeleteUnneccessaryOriginal(Artwork artwork, ILogger? logger);
+    public ValueTask<bool> TryConvertAsync(FileInfo file, ILogger? logger, CancellationToken cancellationToken);
 }
