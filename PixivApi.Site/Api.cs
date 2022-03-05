f1e305e4318538e51ffa45f9b355eecc5760f6a8
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-06 01:25:42 +0900

    add constructor

--- PixivApi.Site/Api.cs
+++ PixivApi.Site/Api.cs
@@ -2,7 +2,12 @@
 
 public class Api
 {
+    private readonly ConfigSettings configSettings;
+    private readonly HttpClient client;
+
     public Api(ConfigSettings configSettings, HttpClient client)
     {
+        this.configSettings = configSettings;
+        this.client = client;
     }
 }
