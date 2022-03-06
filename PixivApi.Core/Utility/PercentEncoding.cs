b1ae77239ec7f560b43d3b63018b84ce0ac72201
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-07 03:46:44 +0900

    add utf8 support

--- PixivApi.Core/Utility/PercentEncoding.cs
+++ PixivApi.Core/Utility/PercentEncoding.cs
@@ -2,7 +2,7 @@
 
 namespace PixivApi.Core;
 
-public readonly struct PercentEncoding : ISpanFormattable, IEquatable<PercentEncoding>
+public readonly struct PercentEncoding : ISpanFormattable
 {
     private readonly string text;
 
@@ -15,26 +15,100 @@ public readonly struct PercentEncoding : ISpanFormattable, IEquatable<PercentEnc
 
     public string ToString(string? format, IFormatProvider? formatProvider) => $"{text}";
 
+    public static void Encode(ref Utf8ValueStringBuilder builder, SpanRuneEnumerator enumerator)
+    {
+        Span<byte> span = stackalloc byte[4];
+        while (enumerator.MoveNext())
+        {
+            Encode(ref builder, enumerator.Current, span);
+        }
+    }
+
+    public static void Encode(ref Utf8ValueStringBuilder builder, Rune c) => Encode(ref builder, c, stackalloc byte[4]);
+
+    public static void Encode(ref Utf8ValueStringBuilder builder, ReadOnlySpan<char> text) => Encode(ref builder, text.EnumerateRunes());
+
+    private static void Encode(ref Utf8ValueStringBuilder builder, Rune c, Span<byte> span)
+    {
+        Span<byte> destination;
+        var value = c.Value;
+        if (c.IsAscii)
+        {
+            switch (value)
+            {
+                case ':':
+                case ';':
+                case ' ':
+                case '%':
+                case '=':
+                case '+':
+                case '*':
+                case '(':
+                case ')':
+                case '\'':
+                case '"':
+                case '&':
+                case '$':
+                case '!':
+                case '@':
+                case '[':
+                case ']':
+                case '#':
+                case '?':
+                case '/':
+                    destination = builder.GetSpan(3);
+                    destination[0] = (byte)'%';
+                    (destination[1], destination[2]) = CalcBytePair(value);
+                    builder.Advance(3);
+                    return;
+                default:
+                    if (value < 32)
+                    {
+                        destination = builder.GetSpan(3);
+                        destination[0] = (byte)'%';
+                        if (value < 16)
+                        {
+                            destination[1] = (byte)'0';
+                        }
+                        else
+                        {
+                            destination[1] = (byte)'1';
+                            value ^= 16;
+                        }
+
+                        destination[2] = CalcNumberByte(value);
+                        builder.Advance(3);
+                    }
+                    else
+                    {
+                        destination = builder.GetSpan(1);
+                        destination[0] = (byte)value;
+                        builder.Advance(1);
+                    }
+
+                    return;
+            }
+        }
+
+        var length = c.EncodeToUtf8(span);
+        var written = length * 3;
+        destination = builder.GetSpan(written);
+        var tmp = span[..length];
+        foreach (var v in tmp)
+        {
+            destination[0] = (byte)'%';
+            destination[1] = CalcNumberByte(v >> 4);
+            destination[2] = CalcNumberByte(v & 15);
+            destination = destination[3..];
+        }
+
+        builder.Advance(written);
+    }
+
     public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider)
     {
         charsWritten = 0;
         var enumerator = text.AsSpan().EnumerateRunes();
-        static char CalcNumber(int v)
-        {
-            if (v >= 10)
-            {
-                return (char)('A' - 10 + v);
-            }
-
-            return (char)('0' + v);
-        }
-
-        static (char, char) Calc(Rune rune)
-        {
-            var c = rune.Value;
-            return (CalcNumber(c >> 4), CalcNumber(c & 15));
-        }
-
         uint bytes = 0;
         var span = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref bytes, 1));
         while (enumerator.MoveNext())
@@ -71,7 +145,7 @@ public readonly struct PercentEncoding : ISpanFormattable, IEquatable<PercentEnc
 
                         charsWritten += 3;
                         destination[0] = '%';
-                        (destination[1], destination[2]) = Calc(c);
+                        (destination[1], destination[2]) = CalcCharPair(c.Value);
                         destination = destination[3..];
                         continue;
                     default:
@@ -99,8 +173,8 @@ public readonly struct PercentEncoding : ISpanFormattable, IEquatable<PercentEnc
             foreach (var v in tmp)
             {
                 destination[0] = '%';
-                destination[1] = CalcNumber(v >> 4);
-                destination[2] = CalcNumber(v & 15);
+                destination[1] = CalcNumberChar(v >> 4);
+                destination[2] = CalcNumberChar(v & 15);
                 destination = destination[3..];
             }
         }
@@ -108,11 +182,27 @@ public readonly struct PercentEncoding : ISpanFormattable, IEquatable<PercentEnc
         return true;
     }
 
-    public override bool Equals(object? obj) => obj is PercentEncoding other && Equals(other);
+    private static char CalcNumberChar(int v)
+    {
+        if (v >= 10)
+        {
+            return (char)('A' - 10 + v);
+        }
 
-    public static bool operator ==(PercentEncoding left, PercentEncoding right) => left.Equals(right);
+        return (char)('0' + v);
+    }
 
-    public static bool operator !=(PercentEncoding left, PercentEncoding right) => !(left == right);
+    private static byte CalcNumberByte(int v)
+    {
+        if (v >= 10)
+        {
+            return (byte)('A' - 10 + v);
+        }
 
-    public override int GetHashCode() => text.GetHashCode();
+        return (byte)('0' + v);
+    }
+
+    private static (char, char) CalcCharPair(int value) => (CalcNumberChar(value >> 4), CalcNumberChar(value & 15));
+
+    private static (byte, byte) CalcBytePair(int value) => (CalcNumberByte(value >> 4), CalcNumberByte(value & 15));
 }
