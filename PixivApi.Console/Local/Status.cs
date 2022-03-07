7982c5dde34788795bb7a1f46d27eb84418ef92c
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-07 23:33:01 +0900

    add status command

---
+++ PixivApi.Console/Local/Status.cs
@@ -0,0 +1,22 @@
+﻿namespace PixivApi.Console;
+
+public partial class LocalClient
+{
+    [Command("status", "")]
+    public async ValueTask StatusAsync()
+    {
+        var token = Context.CancellationToken;
+        if (token.IsCancellationRequested || string.IsNullOrWhiteSpace(configSettings.DatabaseFilePath))
+        {
+            return;
+        }
+
+        var database = await IOUtility.MessagePackDeserializeAsync<DatabaseFile>(configSettings.DatabaseFilePath, token).ConfigureAwait(false);
+        if (database is null)
+        {
+            return;
+        }
+
+        logger.LogInformation($"Artwork: {database.ArtworkDictionary.Count} User: {database.UserDictionary.Count}\nTag: {database.TagSet.Reverses.Count} Tool: {database.ToolSet.Reverses.Count}");
+    }
+}
