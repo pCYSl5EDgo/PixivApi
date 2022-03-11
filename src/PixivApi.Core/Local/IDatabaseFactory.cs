59a023d988d332988df423cb53f02be14c768b89
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-11 20:52:07 +0900

    maybe null

--- src/PixivApi.Core/Local/IDatabaseFactory.cs
+++ src/PixivApi.Core/Local/IDatabaseFactory.cs
@@ -4,5 +4,5 @@ public interface IDatabaseFactory : IAsyncDisposable
 {
     ValueTask<IDatabase> RentAsync(CancellationToken token);
 
-    void Return([MaybeNull] ref IDatabase database);
+    void Return([MaybeNull] ref IDatabase database) => database = null;
 }
