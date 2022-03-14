c26b8273c098636639b68135667b3a59135b5760
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-14 23:59:58 +0900

    init and shutdown and index

--- src/PixivApi.Core.SqliteDatabase/DatabaseFactory.cs
+++ src/PixivApi.Core.SqliteDatabase/DatabaseFactory.cs
@@ -12,10 +12,12 @@ public sealed class DatabaseFactory : IDatabaseFactory
     public DatabaseFactory(ConfigSettings configSettings)
     {
         Batteries_V2.Init();
+        raw.sqlite3_initialize();
         path = configSettings.DatabaseFilePath ?? throw new NullReferenceException();
     }
 
     private readonly ConcurrentBag<Database> Returned = new();
+    private ulong index = 0;
 
     public ValueTask<IDatabase> RentAsync(CancellationToken token)
     {
@@ -26,7 +28,7 @@ public sealed class DatabaseFactory : IDatabaseFactory
 
         if (!Returned.TryTake(out var database))
         {
-            database = new(path);
+            database = new(path, Interlocked.Increment(ref index));
         }
 
         return ValueTask.FromResult<IDatabase>(database);
@@ -39,6 +41,7 @@ public sealed class DatabaseFactory : IDatabaseFactory
             database.Dispose();
         }
 
+        raw.sqlite3_shutdown();
         return ValueTask.CompletedTask;
     }
 
