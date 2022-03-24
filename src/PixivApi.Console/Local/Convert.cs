8ff131f2a53e6f1c3d124aef1018e7c72ea45422
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-25 05:19:45 +0900

    convert from messagepack to sqlite

--- src/PixivApi.Console/Local/Convert.cs
+++ src/PixivApi.Console/Local/Convert.cs
@@ -6,91 +6,65 @@ public partial class LocalClient
     public async ValueTask ConvertAsync()
     {
         var token = Context.CancellationToken;
-        var outputFactory = Context.ServiceProvider.GetRequiredService<Core.SqliteDatabase.DatabaseFactory>();
-        var output = await outputFactory.RentAsync(token).ConfigureAwait(false);
         var input = await databaseFactory.RentAsync(token).ConfigureAwait(false);
         try
         {
-            var users = await input.EnumerateUserAsync(token).ToArrayAsync(token).ConfigureAwait(false);
-            var artworks = await input.EnumerateArtworkAsync(token).ToArrayAsync(token).ConfigureAwait(false);
-            var tagDictionary = new Dictionary<string, ulong>();
-            var toolDictionary = new Dictionary<string, ulong>();
-
-            static void Increment(Dictionary<string, ulong> dictionary, string? text)
+            var sqlFactory = Context.ServiceProvider.GetRequiredService<Core.SqliteDatabase.DatabaseFactory>();
+            var output = await sqlFactory.RentAsync(token).ConfigureAwait(false);
+            try
             {
-                if (string.IsNullOrEmpty(text))
+                logger.LogInformation("Start register tags.");
+                var tagCount = 0;
+                await foreach (var (tag, _) in input.EnumerateTagAsync(token))
                 {
-                    return;
+                    await output.RegisterTagAsync(tag, token).ConfigureAwait(false);
+                    if ((++tagCount & 1023) == 0)
+                    {
+                        System.Console.Write($"{VirtualCodes.DeleteLine1}Count: {tagCount}");
+                    }
                 }
 
-                ++CollectionsMarshal.GetValueRefOrAddDefault(dictionary, text, out _);
-            }
-
-            foreach (var item in users)
-            {
-                if (item.ExtraTags is { Length: > 0 } tags)
+                logger.LogInformation("Start register tools.");
+                var toolCount = 0;
+                await foreach (var (tool, _) in input.EnumerateToolAsync(token))
                 {
-                    foreach (var number in tags)
+                    await output.RegisterToolAsync(tool, token).ConfigureAwait(false);
+                    if ((++toolCount & 1023) == 0)
                     {
-                        var text = await input.GetTagAsync(number, token).ConfigureAwait(false);
-                        Increment(tagDictionary, text);
+                        System.Console.Write($"{VirtualCodes.DeleteLine1}Count: {toolCount}");
+                    }
+                }
+
+                logger.LogInformation("Start register users.");
+                var userCount = 0;
+                await foreach (var item in input.EnumerateUserAsync(token))
+                {
+                    await output.AddOrUpdateAsync(item.Id, async _ => item, static (_, _) => throw new NotImplementedException(), token);
+                    if ((++userCount & 1023) == 0)
+                    {
+                        System.Console.Write($"{VirtualCodes.DeleteLine1}Count: {userCount}");
+                    }
+                }
+
+                logger.LogInformation("Start register artworks.");
+                var artworkCount = 0;
+                await foreach (var item in input.EnumerateArtworkAsync(token))
+                {
+                    await output.AddOrUpdateAsync(item.Id, async _ => item, static (_, _) => throw new NotImplementedException(), token);
+                    if ((++artworkCount & 1023) == 0)
+                    {
+                        System.Console.Write($"{VirtualCodes.DeleteLine1}Count: {artworkCount}");
                     }
                 }
             }
-
-            foreach (var item in artworks)
+            finally
             {
-                foreach (var number in item.ExtraFakeTags ?? Array.Empty<uint>())
-                {
-                    var text = await input.GetTagAsync(number, token).ConfigureAwait(false);
-                    Increment(tagDictionary, text);
-                }
-
-                foreach (var number in item.Tags)
-                {
-                    var text = await input.GetTagAsync(number, token).ConfigureAwait(false);
-                    Increment(tagDictionary, text);
-                }
-
-                foreach (var number in item.ExtraTags ?? Array.Empty<uint>())
-                {
-                    var text = await input.GetTagAsync(number, token).ConfigureAwait(false);
-                    Increment(tagDictionary, text);
-                }
-
-                foreach (var number in item.Tools)
-                {
-                    var text = await input.GetToolAsync(number, token).ConfigureAwait(false);
-                    Increment(toolDictionary, text);
-                }
-            }
-
-            var tagArray = tagDictionary.Keys.ToArray();
-            var tagCounts = tagDictionary.Values.ToArray();
-            tagCounts.AsSpan().Sort(tagArray.AsSpan(), static (x, y) => y.CompareTo(x));
-            var tagConversion = new Dictionary<uint, uint>(tagArray.Length);
-            foreach (var text in tagArray)
-            {
-                var newId = await output.RegisterTagAsync(text, token).ConfigureAwait(false);
-                var oldId = await input.FindTagAsync(text, token).ConfigureAwait(false) ?? throw new NullReferenceException();
-                tagConversion.Add(oldId, newId);
-            }
-
-            var toolArray = toolDictionary.Keys.ToArray();
-            var toolCounts = toolDictionary.Values.ToArray();
-            toolCounts.AsSpan().Sort(toolArray.AsSpan(), static (x, y) => y.CompareTo(x));
-            var toolConversion = new Dictionary<uint, uint>(toolArray.Length);
-            foreach (var text in toolArray)
-            {
-                var newId = await output.RegisterToolAsync(text, token).ConfigureAwait(false);
-                var oldId = await input.FindToolAsync(text, token).ConfigureAwait(false) ?? throw new NullReferenceException();
-                toolConversion.Add(oldId, newId);
+                sqlFactory.Return(ref output);
             }
         }
         finally
         {
             databaseFactory.Return(ref input);
-            outputFactory.Return(ref output);
         }
     }
 }
