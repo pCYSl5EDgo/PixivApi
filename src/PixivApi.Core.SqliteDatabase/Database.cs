b5facc4f782c9d7a607df2a8697a8e198c869e9d
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-03-30 20:26:38 +0900

    fix select

--- src/PixivApi.Core.SqliteDatabase/Database.cs
+++ src/PixivApi.Core.SqliteDatabase/Database.cs
@@ -529,7 +529,7 @@ internal sealed partial class Database : IExtenededDatabase, ITransactionalDatab
     }
 
     #region Version
-    [StringLiteral.Utf8("SELECT (\"Major\", \"Minor\") FROM \"InfoTable\" ORDER BY \"Major\" DESC, \"Minor\" DESC LIMIT 1")]
+    [StringLiteral.Utf8("SELECT \"Major\", \"Minor\" FROM \"InfoTable\" ORDER BY \"Major\" DESC, \"Minor\" DESC LIMIT 1")]
     private static partial ReadOnlySpan<byte> Literal_Select_Version();
 
     private Version? version;
