04bdb1f3508fa7e635b3dd8e1ea3553d81bff36c
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-04-10 07:00:49 +0900

    support private

--- src/PixivApi.Console/Network/Bookmarks.cs
+++ src/PixivApi.Console/Network/Bookmarks.cs
@@ -6,7 +6,8 @@ public partial class NetworkClient
     public ValueTask DownloadBookmarksOfUserAsync
     (
         [Option("a", ArgumentDescriptions.AddKindDescription)] bool addBehaviour = false,
-        [Option("d")] bool download = false
+        [Option("d")] bool download = false,
+        [Option("p")] bool isPrivate = false
     )
     {
         if (string.IsNullOrWhiteSpace(configSettings.DatabaseFilePath))
@@ -20,7 +21,8 @@ public partial class NetworkClient
             return ValueTask.CompletedTask;
         }
 
-        var url = $"https://{ApiHost}/v1/user/bookmarks/illust?user_id={configSettings.UserId}&restrict=public";
+        var privateOrPublic = isPrivate ? "private" : "public";
+        var url = $"https://{ApiHost}/v1/user/bookmarks/illust?user_id={configSettings.UserId}&restrict={privateOrPublic}";
         return DownloadArtworkResponses(addBehaviour, download, url, Context.CancellationToken);
     }
 }
