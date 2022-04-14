aea37313d96dbfe42483c2c0ef48ff6c972a8d70
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-04-14 12:02:04 +0900

    delete amount

--- src/PixivApi.Console/Local/ClearHidden.cs
+++ src/PixivApi.Console/Local/ClearHidden.cs
@@ -6,60 +6,72 @@ public partial class LocalClient
     public async ValueTask ClearHiddenAsync(
     )
     {
+        var totalByteCount = 0UL;
         var token = Context.CancellationToken;
         var database = await databaseFactory.RentAsync(token).ConfigureAwait(false);
         var finderFacade = Context.ServiceProvider.GetRequiredService<FinderFacade>();
         try
         {
-            static void DeleteFileWithIndex(in HiddenPageValueTuple tuple, IFinderWithIndex finder, ILogger? logger)
+            static ulong DeleteFileWithIndex(in HiddenPageValueTuple tuple, IFinderWithIndex finder, ILogger? logger)
             {
                 var info = finder.Find(tuple.Id, tuple.Extension, tuple.Index);
                 if (info.Exists)
                 {
                     logger?.LogTrace($"Delete: {tuple}");
+                    var length = info.Length;
                     info.Delete();
+                    return (ulong)length;
                 }
+
+                return 0;
             }
 
-            static void DeleteFile(in HiddenPageValueTuple tuple, IFinder finder, ILogger? logger)
+            static ulong DeleteFile(in HiddenPageValueTuple tuple, IFinder finder, ILogger? logger)
             {
                 var info = finder.Find(tuple.Id, tuple.Extension);
                 if (info.Exists)
                 {
                     logger?.LogTrace($"Delete: {tuple}");
+                    var length = info.Length;
                     info.Delete();
+                    return (ulong)length;
                 }
+
+                return 0;
             }
 
-            void Delete(in HiddenPageValueTuple tuple)
+            ulong Delete(in HiddenPageValueTuple tuple)
             {
                 var tmpLogger = logger.IsEnabled(LogLevel.Trace) ? logger : null;
+                ulong length = 0;
                 switch (tuple.Type)
                 {
                     case ArtworkType.Illust:
-                        DeleteFileWithIndex(tuple, finderFacade.IllustThumbnailFinder, tmpLogger);
-                        DeleteFileWithIndex(tuple, finderFacade.IllustOriginalFinder, tmpLogger);
+                        length += DeleteFileWithIndex(tuple, finderFacade.IllustThumbnailFinder, tmpLogger);
+                        length += DeleteFileWithIndex(tuple, finderFacade.IllustOriginalFinder, tmpLogger);
                         break;
                     case ArtworkType.Manga:
-                        DeleteFileWithIndex(tuple, finderFacade.MangaThumbnailFinder, tmpLogger);
-                        DeleteFileWithIndex(tuple, finderFacade.MangaOriginalFinder, tmpLogger);
+                        length += DeleteFileWithIndex(tuple, finderFacade.MangaThumbnailFinder, tmpLogger);
+                        length += DeleteFileWithIndex(tuple, finderFacade.MangaOriginalFinder, tmpLogger);
                         break;
                     case ArtworkType.Ugoira:
-                        DeleteFile(tuple, finderFacade.UgoiraThumbnailFinder, tmpLogger);
-                        DeleteFile(tuple, finderFacade.UgoiraOriginalFinder, tmpLogger);
-                        DeleteFile(tuple, finderFacade.UgoiraZipFinder, tmpLogger);
+                        length += DeleteFile(tuple, finderFacade.UgoiraThumbnailFinder, tmpLogger);
+                        length += DeleteFile(tuple, finderFacade.UgoiraOriginalFinder, tmpLogger);
+                        length += DeleteFile(tuple, finderFacade.UgoiraZipFinder, tmpLogger);
                         break;
                     case ArtworkType.None:
                     default:
                         throw new InvalidDataException(tuple.ToString());
                 }
+
+                return length;
             }
 
             if (database is IExtenededDatabase exteneded)
             {
                 await foreach (var tuple in exteneded.EnumerateHiddenPagesAsync(token))
                 {
-                    Delete(tuple);
+                    totalByteCount += Delete(tuple);
                 }
             }
             else
@@ -76,7 +88,7 @@ public partial class LocalClient
                         default:
                             for (var i = 0U; i < artwork.PageCount; i++)
                             {
-                                Delete(new(artwork.Id, i, artwork.Type, artwork.Extension, artwork.ExtraHideReason));
+                                totalByteCount += Delete(new(artwork.Id, i, artwork.Type, artwork.Extension, artwork.ExtraHideReason));
                             }
                             continue;
                     }
@@ -117,7 +129,7 @@ public partial class LocalClient
                         default:
                             for (var i = 0U; i < artwork.PageCount; i++)
                             {
-                                Delete(new(artwork.Id, i, artwork.Type, artwork.Extension, reason));
+                                totalByteCount += Delete(new(artwork.Id, i, artwork.Type, artwork.Extension, reason));
                             }
                             continue;
                     }
@@ -132,7 +144,7 @@ public partial class LocalClient
                                 case HideReason.TemporaryHidden:
                                     continue;
                                 default:
-                                    Delete(new(artwork.Id, pair.Key, artwork.Type, artwork.Extension, pair.Value));
+                                    totalByteCount += Delete(new(artwork.Id, pair.Key, artwork.Type, artwork.Extension, pair.Value));
                                     break;
                             }
                         }
@@ -144,5 +156,7 @@ public partial class LocalClient
         {
             databaseFactory.Return(ref database);
         }
+
+        logger.LogInformation($"Total Delete File Amount: {ByteAmountUtility.ToDisplayable(totalByteCount)}");
     }
 }
