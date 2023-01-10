074ac0e83579b964677acf5c9eb49f7a148bf57a
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2023-01-10 18:00:17 +0900

    Add delete-file command

---
+++ src/PixivApi.Console/Local/DeleteFile.cs
@@ -0,0 +1,93 @@
+﻿namespace PixivApi.Console;
+
+public partial class LocalClient
+{
+    [Command("delete-file", "")]
+    public async ValueTask DeleteFileAsync(
+        [Option(0, ArgumentDescriptions.FilterDescription)] string? filter = null
+    )
+    {
+        var errorNotRedirected = !System.Console.IsErrorRedirected;
+        var token = Context.CancellationToken;
+        filter ??= configSettings.ArtworkFilterFilePath;
+        var database = await databaseFactory.RentAsync(token).ConfigureAwait(false);
+        if (errorNotRedirected)
+        {
+            System.Console.Error.Write($"{VirtualCodes.DeleteLine1}Load database.");
+        }
+
+        try
+        {
+            var artworkFilter = string.IsNullOrWhiteSpace(filter) ? null : await filterFactory.CreateAsync(database, new FileInfo(filter), token).ConfigureAwait(false);
+            if (errorNotRedirected)
+            {
+                System.Console.Error.Write($"{VirtualCodes.DeleteLine1}Load filter.");
+            }
+
+            if (artworkFilter is null or { Count: <= 0 })
+            {
+                logger.LogInformation("0");
+                return;
+            }
+
+            
+
+            if (token.IsCancellationRequested)
+            {
+                return;
+            }
+
+            var finder = Context.ServiceProvider.GetRequiredService<FinderFacade>();
+            if (finder is null)
+            {
+                return;
+            }
+
+            var count = 0UL;
+            await foreach (var artwork in database.FilterAsync(artworkFilter, token).ConfigureAwait(false))
+            {
+                if (errorNotRedirected)
+                {
+                    System.Console.Error.Write($"{VirtualCodes.DeleteLine1}Delete Count: {++count}");
+                }
+
+                switch (artwork.Type)
+                {
+                    case ArtworkType.Illust:
+                        for (var index = 0U; index < artwork.PageCount; index++)
+                        {
+                            if (token.IsCancellationRequested)
+                            {
+                                break;
+                            }
+                            finder.IllustOriginalFinder.Find(artwork.Id, artwork.Extension, index).Delete();
+                            finder.IllustThumbnailFinder.Find(artwork.Id, artwork.Extension, index).Delete();
+                        }
+                        break;
+                    case ArtworkType.Manga:
+                        for (var index = 0U; index < artwork.PageCount; index++)
+                        {
+                            if (token.IsCancellationRequested)
+                            {
+                                break;
+                            }
+                            finder.IllustOriginalFinder.Find(artwork.Id, artwork.Extension, index).Delete();
+                            finder.IllustThumbnailFinder.Find(artwork.Id, artwork.Extension, index).Delete();
+                        }
+                        break;
+                    case ArtworkType.Ugoira:
+                        finder.UgoiraOriginalFinder.Find(artwork.Id, artwork.Extension).Delete();
+                        finder.UgoiraThumbnailFinder.Find(artwork.Id, artwork.Extension).Delete();
+                        finder.UgoiraZipFinder.Find(artwork.Id, artwork.Extension).Delete();
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+        finally
+        {
+            databaseFactory.Return(ref database);
+        }
+    }
+}
