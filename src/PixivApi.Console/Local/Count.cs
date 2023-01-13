5cfa67a457ae01e5faeca1cb686a906920d42496
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2023-01-13 12:30:24 +0900

    count size

--- src/PixivApi.Console/Local/Count.cs
+++ src/PixivApi.Console/Local/Count.cs
@@ -92,4 +92,147 @@ public partial class LocalClient
             databaseFactory.Return(ref database);
         }
     }
+
+    [Command("count-size", "")]
+    public async ValueTask CountSizeAsync(
+        [Option(0, ArgumentDescriptions.FilterDescription)] string? filter = null,
+        [Option("mask")] byte maskPowerOf2 = 10
+    )
+    {
+        var errorNotRedirected = !System.Console.IsErrorRedirected;
+        var token = Context.CancellationToken;
+        filter ??= configSettings.ArtworkFilterFilePath;
+        if (errorNotRedirected)
+        {
+            System.Console.Error.Write($"{VirtualCodes.DeleteLine1}Load database.");
+        }
+
+        var count = 0UL;
+        var fileCount = 0UL;
+
+        var database = await databaseFactory.RentAsync(token).ConfigureAwait(false);
+        try
+        {
+            var artworkFilter = string.IsNullOrWhiteSpace(filter) ? null : await filterFactory.CreateAsync(database, new FileInfo(filter), token).ConfigureAwait(false);
+            if (artworkFilter is null)
+            {
+                return;
+            }
+
+            artworkFilter.FileExistanceFilter = default;
+            artworkFilter.Count = default;
+            artworkFilter.Offset = default;
+            artworkFilter.Order = ArtworkOrderKind.None;
+            if (errorNotRedirected)
+            {
+                System.Console.Error.Write($"{VirtualCodes.DeleteLine1}Load filter.");
+            }
+
+            if (token.IsCancellationRequested)
+            {
+                return;
+            }
+
+            if (errorNotRedirected)
+            {
+                System.Console.Error.Write($"{VirtualCodes.DeleteLine1}Start collecting.");
+            }
+
+            if (token.IsCancellationRequested)
+            {
+                return;
+            }
+
+            var facade = Context.ServiceProvider.GetRequiredService<FinderFacade>();
+            await foreach (var artwork in database.FilterAsync(artworkFilter, token))
+            {
+                token.ThrowIfCancellationRequested();
+                var add = false;
+                switch (artwork.Type)
+                {
+                    case ArtworkType.Illust:
+                        for (uint i = 0; i < artwork.PageCount; i++)
+                        {
+                            var file = facade.IllustOriginalFinder.Find(artwork.Id, artwork.Extension, i);
+                            if (file.Exists)
+                            {
+                                add = true;
+                                count += (ulong)file.Length;
+                            }
+                        }
+                        for (uint i = 0; i < artwork.PageCount; i++)
+                        {
+                            var file = facade.IllustThumbnailFinder.Find(artwork.Id, artwork.Extension, i);
+                            if (file.Exists)
+                            {
+                                add = true;
+                                count += (ulong)file.Length;
+                            }
+                        }
+                        break;
+                    case ArtworkType.Manga:
+                        for (uint i = 0; i < artwork.PageCount; i++)
+                        {
+                            var file = facade.MangaOriginalFinder.Find(artwork.Id, artwork.Extension, i);
+                            if (file.Exists)
+                            {
+                                add = true;
+                                count += (ulong)file.Length;
+                            }
+                        }
+                        for (uint i = 0; i < artwork.PageCount; i++)
+                        {
+                            var file = facade.MangaThumbnailFinder.Find(artwork.Id, artwork.Extension, i);
+                            if (file.Exists)
+                            {
+                                add = true;
+                                count += (ulong)file.Length;
+                            }
+                        }
+                        break;
+                    case ArtworkType.Ugoira:
+                        {
+                            var zip = facade.UgoiraZipFinder.Find(artwork.Id, artwork.Extension);
+                            if (zip.Exists)
+                            {
+                                add = true;
+                                count += (ulong)zip.Length;
+                            }
+
+                            var original = facade.UgoiraOriginalFinder.Find(artwork.Id, artwork.Extension);
+                            if (original.Exists)
+                            {
+                                add = true;
+                                count += (ulong)original.Length;
+                            }
+
+                            var thumbnail = facade.UgoiraThumbnailFinder.Find(artwork.Id, artwork.Extension);
+                            if (thumbnail.Exists)
+                            {
+                                add = true;
+                                count += (ulong)thumbnail.Length;
+                            }
+                        }
+                        break;
+                    default:
+                        continue;
+                }
+
+                if (add)
+                {
+                    fileCount++;
+                }
+            }
+
+            if (errorNotRedirected)
+            {
+                System.Console.Error.Write(VirtualCodes.DeleteLine1);
+            }
+        }
+        finally
+        {
+            logger.LogInformation($"File Size: {ByteAmountUtility.ToDisplayable(count)}\nFile Count: {fileCount}");
+            databaseFactory.Return(ref database);
+        }
+    }
 }
